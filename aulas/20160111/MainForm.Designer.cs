60f9fdec7dec7f749432cbfa03e1a82a8a62d420
João Trindade <jtrindade@cc.isel.ipl.pt>
2016-01-15 01:05:48 +0000

    Aula de 2016-01-11

---
+++ aulas/20160111/MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.prgFeedback = new System.Windows.Forms.ProgressBar();
+            this.butDoWrong = new System.Windows.Forms.Button();
+            this.butDoOldStyle = new System.Windows.Forms.Button();
+            this.butJustDoIt = new System.Windows.Forms.Button();
+            this.butDoItIllegal = new System.Windows.Forms.Button();
+            this.butDoItRaw = new System.Windows.Forms.Button();
+            this.bgwWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(13, 12);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(409, 20);
+            this.txtResult.TabIndex = 0;
+            // 
+            // prgFeedback
+            // 
+            this.prgFeedback.Location = new System.Drawing.Point(13, 39);
+            this.prgFeedback.Name = "prgFeedback";
+            this.prgFeedback.Size = new System.Drawing.Size(409, 23);
+            this.prgFeedback.TabIndex = 1;
+            // 
+            // butDoWrong
+            // 
+            this.butDoWrong.Location = new System.Drawing.Point(13, 69);
+            this.butDoWrong.Name = "butDoWrong";
+            this.butDoWrong.Size = new System.Drawing.Size(75, 23);
+            this.butDoWrong.TabIndex = 2;
+            this.butDoWrong.Text = "Do it wrong!";
+            this.butDoWrong.UseVisualStyleBackColor = true;
+            this.butDoWrong.Click += new System.EventHandler(this.butDoWrong_Click);
+            // 
+            // butDoOldStyle
+            // 
+            this.butDoOldStyle.Location = new System.Drawing.Point(256, 70);
+            this.butDoOldStyle.Name = "butDoOldStyle";
+            this.butDoOldStyle.Size = new System.Drawing.Size(85, 23);
+            this.butDoOldStyle.TabIndex = 3;
+            this.butDoOldStyle.Text = "Do it old style!";
+            this.butDoOldStyle.UseVisualStyleBackColor = true;
+            this.butDoOldStyle.Click += new System.EventHandler(this.butDoOldStyle_Click);
+            // 
+            // butJustDoIt
+            // 
+            this.butJustDoIt.Location = new System.Drawing.Point(347, 70);
+            this.butJustDoIt.Name = "butJustDoIt";
+            this.butJustDoIt.Size = new System.Drawing.Size(75, 23);
+            this.butJustDoIt.TabIndex = 4;
+            this.butJustDoIt.Text = "Do it now!";
+            this.butJustDoIt.UseVisualStyleBackColor = true;
+            this.butJustDoIt.Click += new System.EventHandler(this.butDoItNow_Click);
+            // 
+            // butDoItIllegal
+            // 
+            this.butDoItIllegal.Location = new System.Drawing.Point(94, 70);
+            this.butDoItIllegal.Name = "butDoItIllegal";
+            this.butDoItIllegal.Size = new System.Drawing.Size(75, 23);
+            this.butDoItIllegal.TabIndex = 5;
+            this.butDoItIllegal.Text = "Do it illegal!";
+            this.butDoItIllegal.UseVisualStyleBackColor = true;
+            this.butDoItIllegal.Click += new System.EventHandler(this.butDoItIllegal_Click);
+            // 
+            // butDoItRaw
+            // 
+            this.butDoItRaw.Location = new System.Drawing.Point(175, 70);
+            this.butDoItRaw.Name = "butDoItRaw";
+            this.butDoItRaw.Size = new System.Drawing.Size(75, 23);
+            this.butDoItRaw.TabIndex = 6;
+            this.butDoItRaw.Text = "Do it raw!";
+            this.butDoItRaw.UseVisualStyleBackColor = true;
+            this.butDoItRaw.Click += new System.EventHandler(this.butDoItRaw_Click);
+            // 
+            // bgwWorker
+            // 
+            this.bgwWorker.WorkerReportsProgress = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 105);
+            this.Controls.Add(this.butDoItRaw);
+            this.Controls.Add(this.butDoItIllegal);
+            this.Controls.Add(this.butJustDoIt);
+            this.Controls.Add(this.butDoOldStyle);
+            this.Controls.Add(this.butDoWrong);
+            this.Controls.Add(this.prgFeedback);
+            this.Controls.Add(this.txtResult);
+            this.Name = "MainForm";
+            this.Text = "GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ProgressBar prgFeedback;
+        private System.Windows.Forms.Button butDoWrong;
+        private System.Windows.Forms.Button butDoOldStyle;
+        private System.Windows.Forms.Button butJustDoIt;
+        private System.Windows.Forms.Button butDoItIllegal;
+        private System.Windows.Forms.Button butDoItRaw;
+        private System.ComponentModel.BackgroundWorker bgwWorker;
+    }
+}
+
---
+++ aulas/20160111/MainForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            PrepareForOldStyle();
+        }
+
+        // GUI control thread blocked.
+        private void butDoWrong_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = true;
+            txtResult.Text = "working...";
+            prgFeedback.Value = 0;
+            Refresh();
+            for (int i = 0; i < 5; ++i) {
+                Thread.Sleep(2000);
+                prgFeedback.Value = (i + 1) * 20;
+            }
+            txtResult.Text = "DONE";
+        }
+
+        // Illegal access from secondary thread.
+        private void butDoItIllegal_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            txtResult.Text = "working...";
+            prgFeedback.Value = 0;
+            ThreadPool.QueueUserWorkItem((_) =>
+            {
+                for (int i = 0; i < 5; ++i)
+                {
+                    Thread.Sleep(2000);
+                    prgFeedback.Value = (i + 1) * 20;
+                }
+                txtResult.Text = "DONE";
+            });
+        }
+
+        // Updates UI in the right thread.
+        private void butDoItRaw_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = true;
+            txtResult.Text = "working...";
+            prgFeedback.Value = 0;
+            Control mainCtx = txtResult;
+            ThreadPool.QueueUserWorkItem((_) =>
+            {
+                for (int i = 0; i < 5; ++i)
+                {
+                    Thread.Sleep(2000);
+                    int percentage = (i + 1) * 20;
+                    mainCtx.BeginInvoke((Action)(() => {
+                        prgFeedback.Value = percentage;
+                    }));
+                }
+                mainCtx.BeginInvoke((Action)(() => {
+                    txtResult.Text = "DONE";
+                }));
+            });
+        }
+
+        private void PrepareForOldStyle()
+        {
+            bgwWorker.DoWork += (src, args) =>
+            {
+                for (int i = 0; i < 5; ++i)
+                {
+                    Thread.Sleep(2000);
+                    bgwWorker.ReportProgress((i + 1) * 20);
+                }
+                args.Result = "DONE";
+            };
+            bgwWorker.ProgressChanged += (src, args) =>
+            {
+                prgFeedback.Value = args.ProgressPercentage;
+            };
+            bgwWorker.RunWorkerCompleted += (src, args) =>
+            {
+                txtResult.Text = (string)args.Result;
+            };
+        }
+
+        // Uses a BackgroundWorker
+        private void butDoOldStyle_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = true;
+            txtResult.Text = "working...";
+            prgFeedback.Value = 0;
+            bgwWorker.RunWorkerAsync();
+        }
+
+        // Using TPL
+        private async void butDoItNow_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = true;
+            txtResult.Text = "working...";
+            prgFeedback.Value = 0;
+            // SynchronizationContext.SetSynchronizationContext(null);
+            txtResult.Text = await WorkAsync(new Progress<int>((val) => { prgFeedback.Value = val; }));
+        }
+
+        private Task<string> WorkAsync(IProgress<int> prog) {
+            return Task.Run(() => {
+                for (int i = 0; i < 5; ++i)
+                {
+                    Thread.Sleep(2000);
+                    prog.Report((i + 1) * 20);
+                }
+                return "DONE";
+            });
+        }
+
+        // Back to the original code (corrected).
+        private async void _butDoItNow_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = true;
+            txtResult.Text = "working...";
+            prgFeedback.Value = 0;
+            for (int i = 0; i < 5; ++i)
+            {
+                await Task.Delay(2000);
+                prgFeedback.Value = (i + 1) * 20;
+            }
+            txtResult.Text = "DONE";
+        }
+    }
+}
